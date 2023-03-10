7fb6c7718a13d9a215e337b62667210eea706ac4
lohi212 <suvvarilohita@gmail.com>
2023-03-10 15:21:31 +0530

    feat(routing): react-navigate

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import uuid from "react-native-uuid";
-import AppHeader from "./src/components/AppHeader";
 import Main from "./src/components/Main";
-import NotesList from "./src/components/NotesList";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   // console.log(uuid.v4());
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <StatusBar style="auto" />
+    <SafeAreaView>
+      <NavigationContainer>
         <Main />
-      </SafeAreaView>
-    </View>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import AppHeader from "./AppHeader";
 import NotesList from "./NotesList";
 
 const Main = () => {
   return (
-    <>
+    <View style={styles.container}>
       <AppHeader />
       <NotesList />
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // position: "relative",
+    height: "100%",
+  },
+});
 
 export default Main;
